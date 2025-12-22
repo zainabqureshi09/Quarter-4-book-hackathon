b3355b85581048bdb2f953daf9b397d252b4d872
zainabqureshi09 <qayaz9465@gmail.com>
2025-12-22 23:02:28 +0500

    Clean repo: ignore build and node_modules, fix Vercel setup

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\Zainabayaz\\OneDrive\\Desktop\\hachathon1\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\Zainabayaz\\OneDrive\\Desktop\\hachathon1\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\Zainabayaz\\OneDrive\\Desktop\\hachathon1\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\Zainabayaz\\OneDrive\\Desktop\\hachathon1\\src\\css\\custom.css"),
-];
--- build/assets/js/aba21aa0.a4c08b80.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkphysical_ai_robotics_textbook=globalThis.webpackChunkphysical_ai_robotics_textbook||[]).push([[742],{7093(o){o.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
